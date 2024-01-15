40c4fc6f42e85e7bbb5a46ff7c93ec1406254725
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-15 17:05:44 +0530

    Initial Commit

---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Yohan on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
